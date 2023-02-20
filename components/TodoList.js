e002e5d4649b26f5cc856b02331a641a3a878dca
Emmett Naughton <enaughton5@gmail.com>
2023-02-20 10:55:57 -0500

    edit to courses tables

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,119 +1,132 @@
-import { useState, useEffect } from 'react'
-import { supabase } from '../lib/initSupabase'
+import { useState, useEffect } from "react";
+import { supabase } from "../lib/initSupabase";
 
 export default function Todos({ user }) {
-  const [todos, setTodos] = useState([])
-  const [newTaskText, setNewTaskText] = useState('')
-  const [errorText, setError] = useState('')
+  const [courses, setCourses] = useState([]);
+  const [newCourseTitle, setNewCourseTitle] = useState("");
+  const [errorText, setError] = useState("");
 
   useEffect(() => {
-    fetchTodos()
-  }, [])
+    fetchCourses();
+  }, []);
 
-  const fetchTodos = async () => {
-    let { data: todos, error } = await supabase.from('todos').select('*').order('id', true)
-    if (error) console.log('error', error)
-    else setTodos(todos)
-  }
-  const addTodo = async (taskText) => {
-    let task = taskText.trim()
-    if (task.length) {
-      let { data: todo, error } = await supabase
-        .from('todos')
-        .insert({ task, user_id: user.id })
-        .single()
-      if (error) setError(error.message)
-      else setTodos([...todos, todo])
+  const fetchCourses = async () => {
+    let { data: courses, error } = await supabase
+      .from("courses")
+      .select("*")
+      .order("id", true);
+    if (error) console.log("error", error);
+    else setCourses(courses);
+  };
+  const addCourse = async (courseText) => {
+    let title = courseText.trim();
+    if (title.length) {
+      let { data: course, error } = await supabase
+        .from("courses")
+        .insert({ title, user_id: user.id })
+        .single();
+      if (error) setError(error.message);
+      else setCourses([...courses, course]);
     }
-  }
+  };
 
   const deleteTodo = async (id) => {
     try {
-      await supabase.from('todos').delete().eq('id', id)
-      setTodos(todos.filter((x) => x.id != id))
+      await supabase.from("courses").delete().eq("id", id);
+      setCourses(course.filter((x) => x.id != id));
     } catch (error) {
-      console.log('error', error)
+      console.log("error", error);
     }
-  }
+  };
 
   return (
     <div className="w-full">
       <h1 className="mb-12">Todo List.</h1>
       <div className="flex gap-2 my-2">
         <input
-          className="rounded w-full p-2"
+          className="w-full p-2 rounded"
           type="text"
           placeholder="make coffee"
-          value={newTaskText}
+          value={newCourseTitle}
           onChange={(e) => {
-            setError('')
-            setNewTaskText(e.target.value)
+            setError("");
+            setNewCourseTitle(e.target.value);
           }}
         />
-        <button className="btn-black" onClick={() => addTodo(newTaskText)}>
+        <button className="btn-black" onClick={() => addCourse(newCourseTitle)}>
           Add
         </button>
       </div>
       {!!errorText && <Alert text={errorText} />}
-      <div className="bg-white shadow overflow-hidden rounded-md">
+      <div className="overflow-hidden bg-white rounded-md shadow">
         <ul>
-          {todos.map((todo) => (
-            <Todo key={todo.id} todo={todo} onDelete={() => deleteTodo(todo.id)} />
+          {courses.map((course) => (
+            <Course
+              key={course.id}
+              course={course}
+              onDelete={() => deleteTodo(todo.id)}
+            />
           ))}
         </ul>
       </div>
     </div>
-  )
+  );
 }
 
-const Todo = ({ todo, onDelete }) => {
-  const [isCompleted, setIsCompleted] = useState(todo.is_complete)
+const Course = ({ course, onDelete }) => {
+  const [isPublic, setIsPublic] = useState(course.is_public);
 
   const toggle = async () => {
     try {
       const { data, error } = await supabase
-        .from('todos')
+        .from("courses")
         .update({ is_complete: !isCompleted })
-        .eq('id', todo.id)
-        .single()
+        .eq("id", course.id)
+        .single();
       if (error) {
-        throw new Error(error)
+        throw new Error(error);
       }
-      setIsCompleted(data.is_complete)
+      setIsPublic(data.is_public);
     } catch (error) {
-      console.log('error', error)
+      console.log("error", error);
     }
-  }
+  };
 
   return (
     <li
       onClick={(e) => {
-        e.preventDefault()
-        toggle()
+        e.preventDefault();
+        toggle();
       }}
-      className="w-full block cursor-pointer hover:bg-gray-200 focus:outline-none focus:bg-gray-200 transition duration-150 ease-in-out"
+      className="block w-full transition duration-150 ease-in-out cursor-pointer hover:bg-gray-200 focus:outline-none focus:bg-gray-200"
     >
       <div className="flex items-center px-4 py-4 sm:px-6">
-        <div className="min-w-0 flex-1 flex items-center">
-          <div className="text-sm leading-5 font-medium truncate">{todo.task}</div>
+        <div className="flex items-center flex-1 min-w-0">
+          <div className="text-sm font-medium leading-5 truncate">
+            {course.title}
+          </div>
         </div>
         <div>
           <input
             className="cursor-pointer"
             onChange={(e) => toggle()}
             type="checkbox"
-            checked={isCompleted ? true : ''}
+            checked={isPublic ? true : ""}
           />
         </div>
         <button
           onClick={(e) => {
-            e.preventDefault()
-            e.stopPropagation()
-            onDelete()
+            e.preventDefault();
+            e.stopPropagation();
+            onDelete();
           }}
-          className="w-4 h-4 ml-2 border-2 hover:border-black rounded"
+          className="w-4 h-4 ml-2 border-2 rounded hover:border-black"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="gray">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="gray"
+          >
             <path
               fillRule="evenodd"
               d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
@@ -123,11 +136,11 @@ const Todo = ({ todo, onDelete }) => {
         </button>
       </div>
     </li>
-  )
-}
+  );
+};
 
 const Alert = ({ text }) => (
-  <div className="rounded-md bg-red-100 p-4 my-3">
+  <div className="p-4 my-3 bg-red-100 rounded-md">
     <div className="text-sm leading-5 text-red-700">{text}</div>
   </div>
-)
+);
